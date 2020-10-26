9c84fe45951ab64f20656e677a6ea9a46127dd35
Ana B. E <am.blan.esc@gmail.com>
2020-10-26 21:32:33 +0100

    Second Commit
    Implementación de las clases del modelo completo con JavaBeans

---
+++ src/modelBean/EmployeeBean.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package modelBean;
+
+/**
+ * @author debrax
+ *
+ */
+public class EmployeeBean extends PersonBean {
+
+	// ATTRIBUTES:
+	private int idEmployee;
+	/** Categoría del empleado */
+	private int category;
+	/** Años trabajados por el empleado */
+	private int years;
+
+	/**
+	 * Constructor genérico.
+	 */
+	// CONSTRUCTOR:
+	public EmployeeBean() {
+		super();
+	}
+
+	public EmployeeBean(String name, String dni, char sex, int idEmployee, int category, int year) {
+		super(name, dni, sex);
+		this.idEmployee = idEmployee;
+		this.category = category;
+		this.years = years;
+	}
+
+	// GETTERS & SETTERS:
+	/**
+	 * @return the idEmployee
+	 */
+	public int getIdEmployee() {
+		return idEmployee;
+	}
+
+	/**
+	 * @param idEmployee the idEmployee to set
+	 */
+	public void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public int getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(int category) {
+		this.category = category;
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYears() {
+		return years;
+	}
+
+	/**
+	 * @param year the year to set
+	 */
+	public void setYears(int years) {
+		this.years = years;
+	}
+
+}
